ae45124053737c2cff63e80e8a9d2604ca4bffe6
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2023-02-18 18:41:13 -0500

    feat(frontend): 🎉 Add new frontend project.
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

---
+++ frontend/create-structure.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Create the components directory if it doesn't exist
+mkdir -p src/components
+
+# Create the Header component files
+touch src/components/Header/Header.tsx
+touch src/components/Header/Header.module.css
+
+# Create the Footer component files
+touch src/components/Footer/Footer.tsx
+touch src/components/Footer/Footer.module.css
+
+# Create the App component files
+touch src/App.tsx
+touch src/App.module.css
+
+# Create the index.js file
+touch src/index.tsx
+
+
+# backend/
+# docker/
+# docs/
+# examples/
+# helpers/
+# library/
+# LICENSE
+# private/
+# scripts/
+# services/
+# static/
+# .prettierrc.cjs
+# monorepo-one/common/
+# monorepo-one/frontend/
+# monorepo-one/monorepo-one.code-workspace
+# monorepo-one/rush.json
+# .editorconfig
+# .git/
+# .gitattributes
+# .github/
+# .gitignore
+# .gitmodules
+# Untitled.ipynb
+# .ipynb_checkpoints/
+# README.md
+# .nvmrc
+# .prettierignore
+# project-listing.sh*
+# tests.sh*
+# .vscode/
+# pnpm-workspace.yaml
