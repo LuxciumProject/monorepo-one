f10d85191249e99fecde624d303e425d8f965308
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2023-02-25 21:13:26 -0500

    feat(services): ⚡️ Improve config read capabilits.
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- services/scan-directories/src/config-reader.ts
+++ services/scan-directories/src/config-reader.ts
@@ -22,7 +22,7 @@ interface Config {
   /**
    * A mapping of folder names to their corresponding folder objects.
    */
-  folderRoots: { [folderName: string]: FolderConfig };
+  folderRoots: { [pathAlias: string]: FolderConfig };
 }
 
 /**
@@ -41,16 +41,16 @@ export const getConfig = (configPath: string): Config => {
 /**
  * Retrieves the full path to a folder on the computer from the configuration object.
  *
- * @param folderName The name of the folder to retrieve the path for.
+ * @param pathAlias The name of the folder to retrieve the path for.
  * @param config The configuration object.
  * @returns The full path to the folder on the computer, or undefined if the folder name is not found in the configuration object.
  */
 export const getFolderPath = (
-  folderName: string,
+  pathAlias: string,
   config: Config,
   rootPath: string
 ): string | undefined => {
-  const folderConfig = config.folderRoots[folderName];
+  const folderConfig = config.folderRoots[pathAlias];
   if (folderConfig) {
     return join(rootPath, folderConfig.path);
   } else {
@@ -58,25 +58,13 @@ export const getFolderPath = (
   }
 };
 
-// const configPath = '/run/media/luxcium/x250Mio/config.json'; // Replace with the actual path to your config file
-// const rootPath = '/'; // Replace with the path to the root directory of the folders in the configuration object
-// const config = getConfig(configPath);
-
-// const dataFolderPath = getFolderPath('data', config, rootPath);
-// if (dataFolderPath) {
-//   console.log(`The full path to the data folder is: ${dataFolderPath}`);
-// } else {
-//   console.log(`The data folder was not found in the configuration file.`);
-// }
-
 export function getDataPath(
-  configPath = '/run/media/luxcium/x250Mio/config.json',
+  pathAlias = 'data',
+  configPath = '/projects/configs/config.json',
   rootPath = '/'
 ): string {
-  // const configPath = '/run/media/luxcium/x250Mio/config.json'; // Replace with the actual path to your config file
-  // const rootPath = '/'; // Replace with the path to the root directory of the folders in the configuration object
   const config = getConfig(configPath);
-  const folderPath = getFolderPath('data', config, rootPath);
+  const folderPath = getFolderPath(pathAlias, config, rootPath);
   console.log(`folderPath: ${folderPath}`);
-  return getFolderPath('data', config, rootPath) || '';
+  return getFolderPath(pathAlias, config, rootPath) || '';
 }
