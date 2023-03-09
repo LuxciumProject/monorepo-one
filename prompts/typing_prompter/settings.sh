042d5d4201c7f8c351c0f12aa4a92e25b6cb3ccc
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2023-03-09 08:38:16 -0500

    Try different settings.
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- prompts/typing_prompter/settings.sh
+++ prompts/typing_prompter/settings.sh
@@ -5,22 +5,22 @@
 # Define the minimum and maximum delay time for characters, words, and lines
 
 export char_min_delay=0.005
-export char_max_delay=0.01
+export char_max_delay=0.1
 
-export word_min_delay=0.05
-export word_max_delay=0.1
+export word_min_delay=0.025125
+export word_max_delay=0.251
 
 export line_min_delay=0.2
 export line_max_delay=0.5
 
-# export char_max_delay=0.5
-# export char_min_delay=0.00001
-
-# export word_max_delay=0.00015
-# export word_min_delay=0.000005
-
-# export line_max_delay=0.00025
-# export line_min_delay=0.00001
+export char_max_delay=0 #.00000000001
+export char_min_delay=0 #.00000000001
+#
+export word_max_delay=0 #.00000000001
+export word_min_delay=0 #.00000000001
+#
+export line_max_delay=0 #.00000000001
+export line_min_delay=0 #.00000000001
 
 # Define the ANSI escape codes for different colors
 export RED='\033[0;31m'
@@ -52,3 +52,22 @@ From the smallest particle to the largest star,
 We're all connected in some way,
 And that thought fills me with wonder and awe,
 As I ponder the universe each day."
+
+echo As I sit and contemplate the universe,
+echo My mind begins to wander and my thoughts disperse.
+echo
+echo I wonder how it all began,
+echo And how it will all end.
+echo
+echo I think about the stars and planets,
+echo And how they spin and bend.
+echo
+echo The mysteries of the universe are many,
+echo And we may never understand them all.
+echo But I find solace in the fact,
+echo That we\'re all a part of this great cosmic ball.
+echo
+echo From the smallest particle to the largest star,
+echo We\'re all connected in some way,
+echo And that thought fills me with wonder and awe,
+echo As I ponder the universe each day.
