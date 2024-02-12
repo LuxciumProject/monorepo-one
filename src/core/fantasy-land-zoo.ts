8cdd94b3ebce07ac5e30fb3d305acf9591d51828
Benjamin Vincent <luxcium@neb401.com>
2024-02-12 10:42:55 -0500

    fix: ⚰️ Remove dead code.
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- src/core/fantasy-land-zoo.ts
+++ src/core/fantasy-land-zoo.ts
@@ -1,19 +1,3 @@
-import { Unboxable } from "../types";
-
-export abstract class ProtoBox<T> implements Unboxable<T> {
-  private readonly _value: T;
-
-  protected constructor(value: T) {
-    this._value = value;
-  }
-  public unbox(): T {
-    return this._value;
-  }
-  get value(): T {
-    return this._value;
-  }
-}
-
 export class Profunctor<Input, Output> {
   constructor(private transformation: (input: Input) => Output) {}
 
