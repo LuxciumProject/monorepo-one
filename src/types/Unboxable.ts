9b02eb771396b9d56977bd9ac0ec07c0d8d99065
Benjamin Vincent <luxcium@neb401.com>
2024-02-12 09:52:15 -0500

    feat: 🎉 🐼🦓🦒🦚 † Scientia es lux principium✨ ™ 🦡🦘🦛🦙🐒🦍🎡🎢🎠🧺🍨🍭🍡🚣🚽🏄‍♀️🚻 🍕🍟🥤🎶🦁🐊🦏🐬🦜🐦🐸🦔🐝🐔🏞🌭🍔
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- src/types/Unboxable.ts
+++ src/types/Unboxable.ts
@@ -1,3 +1,21 @@
+/**
+ * Represents a type that can unbox an internal value T.
+ * @remarks
+ * Must not validate the value here.
+ * Must not change the value here.
+ * Must return value as is.
+ * @typeParam T - The type to be unboxed.
+ */
 export interface Unboxable<T> {
+  /**
+   * Unboxes the internal values T.
+   * @remarks
+   * No parts of unbox's return value should be checked.
+   * Value will be returned as is.
+   * Value will not be validated.
+   * Value will not be changed.
+   * @typeParam T - The type of the value contained in the Functor.
+   * @returns T
+   */
   unbox: () => T;
 }
