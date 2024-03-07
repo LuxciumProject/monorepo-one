fa102b48c01ed57dbebd39abcd646a783da84959
Benjamin Vincent <luxcium@neb401.com>
2024-03-07 10:20:23 -0500

    CHORE: 💥 💥 💥 INCOMING MONOREPO-ONE MERGER
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- library/tools/src/delay/test/delay.test.ts
+++ library/tools/src/delay/test/delay.test.ts
@@ -71,3 +71,72 @@ describe('delay', () => {
     expect(chosenDelay.timeElapsed).toBeLessThanOrEqual(bound);
   });
 });
+import { delay } from '../delay.old';
+
+describe('delay', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.useRealTimers();
+  });
+
+  it('generates a random delay within specified bounds', async () => {
+    const lowerBound = 1000;
+    const upperBound = 2000;
+
+    const delayPromise = delay(lowerBound, upperBound);
+
+    // Resolve the delay promise by advancing timers
+    jest.advanceTimersByTime(upperBound);
+
+    const [chosenDelay] = await delayPromise;
+
+    expect(chosenDelay).toBeGreaterThanOrEqual(lowerBound);
+    expect(chosenDelay).toBeLessThanOrEqual(upperBound);
+  });
+  it('generates a random delay within default value (unspecified values using default) bounds', async () => {
+    const lowerBound = 500;
+    const upperBound = 500;
+
+    const delayPromise = delay();
+
+    // Resolve the delay promise by advancing timers
+    jest.advanceTimersByTime(upperBound);
+
+    const [chosenDelay] = await delayPromise;
+
+    expect(chosenDelay).toBeGreaterThanOrEqual(lowerBound);
+    expect(chosenDelay).toBeLessThanOrEqual(upperBound);
+  });
+
+  it('handles lowerBound greater than upperBound', async () => {
+    const lowerBound = 2000;
+    const upperBound = 1000;
+
+    const delayPromise = delay(lowerBound, upperBound);
+
+    // Resolve the delay promise by advancing timers
+    jest.advanceTimersByTime(lowerBound);
+
+    const [chosenDelay] = await delayPromise;
+
+    expect(chosenDelay).toBeGreaterThanOrEqual(upperBound);
+    expect(chosenDelay).toBeLessThanOrEqual(lowerBound);
+  });
+  it('handles single argument as lowerBound and upperBound', async () => {
+    const bound = 1500;
+
+    const delayPromise = delay(bound);
+
+    // Resolve the delay promise by advancing timers
+    jest.advanceTimersByTime(bound);
+
+    const [chosenDelay] = await delayPromise;
+
+    expect(chosenDelay).toBeGreaterThanOrEqual(bound);
+    expect(chosenDelay).toBeLessThanOrEqual(bound);
+  });
+});
