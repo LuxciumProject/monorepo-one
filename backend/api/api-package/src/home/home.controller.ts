d2c75003cd4eaea950396cc8a8b1416a655c6ae0
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2024-03-08 10:08:09 -0500

    fix: 🚑️ Use of externally-controlled format string.
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- backend/api/api-package/src/home/home.controller.ts
+++ backend/api/api-package/src/home/home.controller.ts
@@ -5,19 +5,19 @@ import { CreateMessageDto } from './dtos/create-message.dto';
 export class HomeController {
   @Get()
   sayHello(@Query() query: any, @Req() req: any) {
-    console.log('HomeController created', query, req);
+    console.log('HomeController created %s %s', query, req);
     return 'HomeController created';
   }
 
   @Post()
   getHello(@Body() body: CreateMessageDto) {
-    console.log(body);
+    console.log('%s', body);
     return body;
   }
   // some comment
   @Get('/:id')
   getHelloById(@Param('id') id: string, @Query() query: any) {
-    console.log(id, query);
+    console.log('%s %s', id, query);
     return id;
   }
 }
