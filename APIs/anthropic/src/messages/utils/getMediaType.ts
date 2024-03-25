b233aa3b340efd1b3707eb23912275d40ad286b8
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2024-03-25 13:26:56 -0400

    Remove dead comments dead code.
    // export const getMediaType: GetMediaType = [
    //   // async function getMediaType(file: Buffer): Promise<ValidMimeType | ''> {
    //   //   const buffer = readFileSync(file);
    //   //   const fileType: ImageFileTypeResult = (await fileTypeFromBuffer(
    //   //     buffer
    //   //   )) as ImageFileTypeResult;
    
    //   //   if (
    //   //     !fileType ||
    //   //     !['image/jpeg', 'image/png', 'image/gif', 'image/webp'].includes(
    //   //       fileType.mime
    //   //     )
    //   //   ) {
    //   //     return '';
    //   //   }
    
    //   //   return fileType ? fileType.mime : '';
    //   // },
    //   function getMediaType(file: string): ValidMimeType | '' {
    //     const ext = extname(file).toLowerCase();
    //     const mediaTypes: { [key: string]: ValidMimeType } = {
    //       '.jpeg': 'image/jpeg',
    //       '.jpg': 'image/jpeg',
    //       '.png': 'image/png',
    //       '.gif': 'image/gif',
    //       '.webp': 'image/webp',
    //     };
    //     return mediaTypes[ext] || '';
    //   },
    // ];
    
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- APIs/anthropic/src/messages/utils/getMediaType.ts
+++ APIs/anthropic/src/messages/utils/getMediaType.ts
@@ -16,34 +16,3 @@ export function getMediaType(file: string): ValidMimeType | '' {
   };
   return mediaTypes[ext] || '';
 }
-
-// export const getMediaType: GetMediaType = [
-//   // async function getMediaType(file: Buffer): Promise<ValidMimeType | ''> {
-//   //   const buffer = readFileSync(file);
-//   //   const fileType: ImageFileTypeResult = (await fileTypeFromBuffer(
-//   //     buffer
-//   //   )) as ImageFileTypeResult;
-
-//   //   if (
-//   //     !fileType ||
-//   //     !['image/jpeg', 'image/png', 'image/gif', 'image/webp'].includes(
-//   //       fileType.mime
-//   //     )
-//   //   ) {
-//   //     return '';
-//   //   }
-
-//   //   return fileType ? fileType.mime : '';
-//   // },
-//   function getMediaType(file: string): ValidMimeType | '' {
-//     const ext = extname(file).toLowerCase();
-//     const mediaTypes: { [key: string]: ValidMimeType } = {
-//       '.jpeg': 'image/jpeg',
-//       '.jpg': 'image/jpeg',
-//       '.png': 'image/png',
-//       '.gif': 'image/gif',
-//       '.webp': 'image/webp',
-//     };
-//     return mediaTypes[ext] || '';
-//   },
-// ];
