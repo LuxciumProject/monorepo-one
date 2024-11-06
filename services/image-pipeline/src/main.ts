c26b001361e98e2f922fa96cf51f77ec6b178778
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2024-11-05 23:35:25 -0500

    feat: add initial structure and comments for image-pipeline service
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- services/image-pipeline/src/main.ts
+++ services/image-pipeline/src/main.ts
@@ -1,6 +1,27 @@
 // main.ts
+// Main entry point for the image-pipeline service for manual testing
+// during development.
 void (async function MAIN() {
   console.log(`at: MAIN from ${__filename}`);
+  // Lests start development here and we will split later in their own
+  // files and modules.
+
+  // 1. Load the configuration including the path to the folder where
+  // the images are stored.
+
+  // 2. Load the images from the folder.
+
+  // 3. Process the images.
+
+  // 4. Save the processed images metadata to a database.
+
+  // 5. Save the processed images to a folder.
+
 
   return void 0;
 })();
+
+// Interfaces goes here
+
+
+// Types goes here
