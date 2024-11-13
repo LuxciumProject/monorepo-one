06e8c8a0eaef1f9f39bf56629d297afc3c452baf
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2024-11-13 04:16:19 -0500

    Add LeftBox class and SettledLeft type for handling rejected promises
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- library/boxed-list/src/Box/Box.ts
+++ library/boxed-list/src/Box/Box.ts
@@ -1,14 +1,12 @@
 // @/Box/Box.ts
 
-import type { IApply, IBox, IChain, IMap, IUnbox, IValue } from '@/types';
+import type { IApply, IBox, IChain, IMap, IUnbox, IValue } from "@/types";
 
 /**
  * This is the doc comment for Box class
- *
  */
 export class Box<T>
-  implements IApply<T>, IChain<T>, IMap<T>, IUnbox<T>, IBox<T>, IValue<T>
-{
+  implements IApply<T>, IChain<T>, IMap<T>, IUnbox<T>, IBox<T>, IValue<T> {
   #value: T;
   private boxedValue: T;
 
@@ -27,19 +25,19 @@ export class Box<T>
     this.boxedValue = value;
     this.#value = this.boxedValue;
 
-    Object.defineProperty(this, 'fantasy-land/map', {
+    Object.defineProperty(this, "fantasy-land/map", {
       value: this.map,
       writable: false,
       enumerable: false,
       configurable: false,
     });
-    Object.defineProperty(this, 'fantasy-land/ap', {
+    Object.defineProperty(this, "fantasy-land/ap", {
       value: this.ap,
       writable: false,
       enumerable: false,
       configurable: false,
     });
-    Object.defineProperty(this, 'fantasy-land/chain', {
+    Object.defineProperty(this, "fantasy-land/chain", {
       value: this.chain,
       writable: false,
       enumerable: false,
@@ -48,17 +46,17 @@ export class Box<T>
     return this;
   }
 
-  public 'fantasy-land/map' = this.map;
+  public "fantasy-land/map" = this.map;
   // public IMap<T> & 'fantasy-land/map' ==============-| map() |-====
   public map<R>(fn: (value: T, index?: -1) => R): Box<R> {
     return Box.of(fn(this.#value, -1));
   }
-  public 'fantasy-land/ap' = this.ap;
+  public "fantasy-land/ap" = this.ap;
   // public IApply<T> ==================================-| ap() |-====
   public ap<R>(c: Box<(val: T) => R>): Box<R> {
-    return this.map<R>(val => c.map(fn => fn(val)).unbox());
+    return this.map<R>((val) => c.map((fn) => fn(val)).unbox());
   }
-  public 'fantasy-land/chain' = this.chain;
+  public "fantasy-land/chain" = this.chain;
   // public IChain<T> ===============================-| chain() |-====
   public chain<R>(fn: (value: T) => Box<R>): Box<R> {
     return this.map<Box<R>>(fn).unbox().box;
