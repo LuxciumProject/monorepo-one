615551dc7a0eb1eec0b6b68c3cffdb3dbcdebd07
Benjamin Vincent (Luxcium) <42672814+Luxcium@users.noreply.github.com>
2026-06-24 05:29:39 -0400

    refactor(BoxedList_new): ✨ update from() method overloads and improve example usage

--- library/boxed-list/src/BoxedList-new/main-BoxedList-new.ts
+++ library/boxed-list/src/BoxedList-new/main-BoxedList-new.ts
@@ -13,11 +13,19 @@ const rawArray1 = doubledList.unbox(); // [2, 4, 6]
 const stringArray = doubledList.unbox(String); // ["2", "4", "6"]
 
 // 4. Instantiation using .from()
-const fun = (x: any) => x + 10;
-const listFromOther = BoxedList_new.from(list2, fun); // Internal: [11, 12, 13]
+const fun = (x: number) => x + 10;
+const listFromOther1 = BoxedList_new.from(list2); // Internal: [11, 12, 13]
+const listFromOther2 = BoxedList_new.from(list2, fun); // Internal: [11, 12, 13]
+
+//  exemple to help me undertand the method relations ship due to the union TVal | TVal[]
+function processData<T, R>(values: T[], fn: (val: T) => R): R[] {
+  return values.map(fn);
+}
+processData([1, 2, 3], x => x + 2);
 
 fun;
 list2;
 rawArray1;
 stringArray;
-listFromOther;
+listFromOther1;
+listFromOther2;
